a8d1e2908fdcd7d21849faf041cceef191d1f903
Zafar <icom@MACKBOOK>
2023-09-22 09:52:13 +0300

    BROKERS: Insert User

---
+++ Sofee.Prosurer/Brokers/Storages/StorageBroker.cs
@@ -0,0 +1,33 @@
+﻿//=================================
+// Copyright (c) Sofee LLC.
+// Powering True Leadership
+//===============================
+
+using EFxceptions;
+using Microsoft.EntityFrameworkCore;
+using Sofee.Prosurer.Models.Users;
+
+namespace Sofee.Prosurer.Brokers.Storages
+{
+    internal class StorageBroker : EFxceptionsContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public StorageBroker() =>
+            this.Database.EnsureCreated();
+
+        public async Task<User> InsertUserAsync(User user)
+        {
+            await this.Users.AddAsync(user);
+            await this.SaveChangesAsync();
+
+            return user;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string connectionString = "Data Source=..\\..\\..\\Sofee.db";
+            optionsBuilder.UseSqlite(connectionString);
+        }
+    }
+}
--- Sofee.Prosurer/Program.cs
+++ Sofee.Prosurer/Program.cs
@@ -3,13 +3,28 @@
 // Powering True Leadership
 //===============================
 
+using Sofee.Prosurer.Brokers.Storages;
+using Sofee.Prosurer.Models.Users;
+
 namespace Sofee.Prosurer
 {
     public class Program
     {
         static async Task Main(string[] args)
         {
+            var user = new User
+            {
+                Id = Guid.NewGuid(),
+                Firstname = "Test",
+                Lastname = "Project",
+                PhoneNumber = "1234567890",
+                Email = "testEmail.dot"
+            };
 
+            using (var storageBroker = new StorageBroker())
+            {
+                await storageBroker.InsertUserAsync(user);
+            }
         }
     }
 }
