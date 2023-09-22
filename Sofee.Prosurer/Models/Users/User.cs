a6b605ded1ab1e73132e28834e1f4ab97fb4c50d
Zafar <icom@MACKBOOK>
2023-09-22 08:57:06 +0300

    FOUNDATIONS: Create Model User

---
+++ Sofee.Prosurer/Models/Users/User.cs
@@ -0,0 +1,20 @@
+﻿//=================================
+// Copyright (c) Sofee LLC.
+// Powering True Leadership
+//===============================
+
+using System;
+
+namespace Sofee.Prosurer.Models.Users
+{
+    internal class User
+    {
+        public Guid Id { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public DateTimeOffSet BirthDate { get; set; }
+        public Guid GroupId { get; set; }
+    }
+}
