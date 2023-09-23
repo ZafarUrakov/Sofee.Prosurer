05e669d7e0258f02db18b1c98eadceab2380ad48
Zafar <icom@MACKBOOK>
2023-09-23 09:41:02 +0300

    FOUNDATIONS: Add Null User Exception

---
+++ Sofee.Prosurer/Models/Users/Exceptions/NullUserException.cs
@@ -0,0 +1,16 @@
+﻿//=================================
+// Copyright (c) Sofee LLC.
+// Powering True Leadership
+//===============================
+
+using Xeptions;
+
+namespace Sofee.Prosurer.Models.Users.Exceptions
+{
+    internal class NullUserException : Xeption
+    {
+        public NullUserException()
+               : base(message: "Client is null")
+        { }
+    }
+}
