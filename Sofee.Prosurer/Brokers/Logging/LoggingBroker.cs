ac2c5aea7ca3b3512ece8a009fa1414db67f1490
Zafar <icom@MACKBOOK>
2023-09-23 09:14:23 +0300

    CONFIGS: Register Logging

---
+++ Sofee.Prosurer/Brokers/Logging/LoggingBroker.cs
@@ -0,0 +1,15 @@
+﻿using Sofee.Prosurer.Models.Users.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sofee.Prosurer.Brokers.Logging
+{
+    internal class LoggingBroker
+    {
+        public void LoggingError(NullUserException nullUserException) =>
+            Console.WriteLine(nullUserException.Message);
+    }
+}
