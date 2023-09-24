d2665f6db98b36c55320d0b508073561e8fb429f
Zafar <icom@MACKBOOK>
2023-09-24 16:29:02 +0300

    FOUNDATIONS: Add Invalid User Exception

---
+++ Sofee.Prosurer/Models/Users/Exceptions/InvalidUserException.cs
@@ -0,0 +1,16 @@
+﻿//=================================
+// Copyright (c) Sofee LLC.
+// Powering True Leadership
+//===============================
+
+using Xeptions;
+
+namespace Sofee.Prosurer.Models.Users.Exceptions
+{
+    internal class InvalidUserException : Xeption
+    {
+        public InvalidUserException()
+            : base(message: "Client is invalid. Fix the errors and try again.")
+        { }
+    }
+}
