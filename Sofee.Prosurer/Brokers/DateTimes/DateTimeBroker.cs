3503a52e7fd13649988c430fd1a1d200567844df
Zafar <icom@MACKBOOK>
2023-09-24 16:26:14 +0300

    BROKERS: DateTimeBroker Initialize

---
+++ Sofee.Prosurer/Brokers/DateTimes/DateTimeBroker.cs
@@ -0,0 +1,28 @@
+﻿//=================================
+// Copyright (c) Sofee LLC.
+// Powering True Leadership
+//===============================
+
+using System;
+
+namespace Sofee.Prosurer.Brokers.DateTimes
+{
+    internal class DateTimeBroker
+    {
+
+        protected dynamic IsInvalidBirthDate(DateTimeOffset birthDate)
+        {
+            var today = DateTimeOffset.UtcNow;
+            var age = today.Year - birthDate.Year;
+
+            if (birthDate.Month > today.Month)
+                age--;
+
+            return new
+            {
+                Condition = age < 6,
+                Message = "User must be 6 years old or older"
+            };
+        }
+    }
+}
