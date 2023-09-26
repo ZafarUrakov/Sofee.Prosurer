f9e14664502b75ec54122acd5c8676d8ca027649
Zafar <icom@MACKBOOK>
2023-09-26 19:56:54 +0300

    POC: Initialize Date Time Broker

--- Sofee.Prosurer/Services/Users/UserService.cs
+++ Sofee.Prosurer/Services/Users/UserService.cs
@@ -7,79 +7,28 @@ using Sofee.Prosurer.Brokers.DateTimes;
 using Sofee.Prosurer.Brokers.Storages;
 using Sofee.Prosurer.Models.Users;
 using Sofee.Prosurer.Models.Users.Exceptions;
-using System;
-using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Sofee.Prosurer.Services.Users
 {
-    internal class UserService : DateTimeBroker
+    internal partial class UserService
     {
         private readonly StorageBroker storageBroker;
+        private readonly DateTimeBroker dateTimeBroker;
 
-        public UserService() =>
+        public UserService()
+        {
             this.storageBroker = new StorageBroker();
+            this.dateTimeBroker = new DateTimeBroker();
+        }
 
         /// <exception cref="NullUserException"></exception>
         /// <exception cref="InvalidUserException"></exception>
         internal Task<User> AddUserAsync(User user)
         {
-            if (user is null)
-                throw new NullUserException();
-
-            Validate(
-                (Rule: IsInvalid(user.Id), Parameter: nameof(User.Id)),
-                (Rule: IsInvalid(user.Firstname), Parameter: nameof(User.Firstname)),
-                (Rule: IsInvalid(user.Email), Parameter: nameof(User.Email)),
-                (Rule: IsInvalid(user.GroupId), Parameter: nameof(User.GroupId)));
-
-            Validate(
-               (Rule: IsInvalidBirthDate(user.BirthDate), Parameter: nameof(User.BirthDate)),
-               (Rule: IsInvalidPhoneNumber(user.PhoneNumber), Parameter: nameof(User.PhoneNumber)),
-               (Rule: IsInvalid(user.Email), Parameter: nameof(User.Email)));
+            ValidateUserOnAdd(user);
 
             return this.storageBroker.InsertUserAsync(user);
-        }
-
-        private dynamic IsInvalid(Guid id) => new
-        {
-            Condition = id == default,
-            Message = "Id is required"
-        };
-
-        private dynamic IsInvalid(string text) => new
-        {
-            Condition = String.IsNullOrWhiteSpace(text),
-            Message = "Text is requred"
-        };
-
-        private dynamic IsInvalideEmail(string email) => new
-        {
-            Condition = Regex.IsMatch(email, @"^(.+)@(.+)$"),
-            Message = "Email is invalide"
-        };
-
-        private dynamic IsInvalidPhoneNumber(string phoneNumber) => new
-        {
-            Condition = !Regex.IsMatch(phoneNumber, "^\\+?[1-9][0-9]{7,14}$"),
-            Message = "PhoneNumber is invalide"
-        };
-
-        private void Validate(params (dynamic Rule, string Parameter)[] validations)
-        {
-            var invalidUserException = new InvalidUserException();
-
-            foreach ((dynamic rule, string parameter) in validations)
-            {
-                if (rule.Condition)
-                {
-                    invalidUserException.UpsertDataList(
-                        key: parameter,
-                        value: rule.Message);
-                }
-            }
-
-            invalidUserException.ThrowIfContainsErrors(); ;
         }
     }
 }
