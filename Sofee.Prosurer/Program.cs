09b6a2df0b41ef23bbf303703ab63a3025f99c0a
Zafar <icom@MACKBOOK>
2023-09-30 12:14:01 +0300

    EXPOSERS: Add User

--- Sofee.Prosurer/Program.cs
+++ Sofee.Prosurer/Program.cs
@@ -3,6 +3,10 @@
 // Powering True Leadership
 //===============================
 
+using Sofee.Prosurer.Brokers.DateTimes;
+using Sofee.Prosurer.Brokers.Storages;
+using Sofee.Prosurer.Models.Users;
+using Sofee.Prosurer.Services.Users;
 using System;
 using System.Threading.Tasks;
 
@@ -12,7 +16,24 @@ namespace Sofee.Prosurer
     {
         static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            UserService userService = new UserService(
+                storageBroker: new StorageBroker(),
+                dateTimeBroker: new DateTimeBroker());
+
+            User user = new User
+            {
+                Id = Guid.NewGuid(),
+                Firstname = "Alberd",
+                Lastname = "Radriges",
+                BirthDate = DateTimeOffset.Parse("1/1/2004"),
+                Email = "Aradriges00@icom.com",
+                PhoneNumber = "+1234567890",
+                GroupId = Guid.NewGuid()
+            };
+
+            var persistedUser = await userService.AddUserAsync(user);
+
+            Console.WriteLine(persistedUser.Id);
         }
     }
 }
