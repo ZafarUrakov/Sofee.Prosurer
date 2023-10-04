a1cb42a1d1a4aee7ee56503b134442044b75cbab
Zafar <icom@MACKBOOK>
2023-10-04 16:04:07 +0300

    ShouldAddUserAsync ->FAIL

---
+++ Sofee.Prosurer.Tests.Unit/Services/Users/UserServiceTests.Logic.Add.cs
@@ -0,0 +1,40 @@
+﻿//=================================
+// Copyright (c) Sofee LLC.
+// Powering True Leadership
+//===============================
+
+using FluentAssertions;
+using Force.DeepCloner;
+using Moq;
+using Sofee.Prosurer.Models.Users;
+using Xunit;
+
+namespace Sofee.Prosurer.Tests.Unit.Services.Users
+{
+    public partial class UserServiceTests
+    {
+        [Fact]
+        public async Task ShouldAddUserAsync()
+        {
+            //given
+            User randomUser = CreateRandomUser();
+            User inputUser = randomUser;
+            User persistedUser = inputUser;
+            User expecteduser = persistedUser.DeepClone();
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.InsertUserAsync(inputUser)).ReturnsAsync(persistedUser);
+
+            //when
+            User actualUser = await this.userService.AddUserAsync(inputUser);
+
+            //then
+            actualUser.Should().BeEquivalentTo(expecteduser);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.InsertUserAsync(inputUser), Times.Once());
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
---
+++ Sofee.Prosurer.Tests.Unit/Services/Users/UserServiceTests.cs
@@ -0,0 +1,43 @@
+﻿//=================================
+// Copyright (c) Sofee LLC.
+// Powering True Leadership
+//===============================
+
+using Moq;
+using Sofee.Prosurer.Brokers.Storages;
+using Sofee.Prosurer.Models.Users;
+using Sofee.Prosurer.Services.Users;
+using Tynamix.ObjectFiller;
+
+namespace Sofee.Prosurer.Tests.Unit.Services.Users
+{
+    public partial class UserServiceTests
+    {
+        private readonly Mock<StorageBroker> storageBrokerMock;
+        private readonly UserService userService;
+
+        public UserServiceTests()
+        {
+            this.storageBrokerMock = new Mock<StorageBroker>();
+
+            this.userService = new UserService(
+                storageBroker: this.storageBrokerMock.Object);
+        }
+
+        private DateTimeOffset GetRandomDateTimeOffset() =>
+            new DateTimeRange(earliestDate: DateTime.UnixEpoch).GetValue();
+
+        private User CreateRandomUser() =>
+            CreateUserFiller(GetRandomDateTimeOffset()).Create();
+
+        private Filler<User> CreateUserFiller(DateTimeOffset dates)
+        {
+            var filler = new Filler<User>();
+
+            filler.Setup()
+                .OnType<DateTimeOffset>().Use(dates);
+
+            return filler;
+        }
+    }
+}
